12130aaa65be3f7a27d9829e40fce584d01d8ae6
Cruyun <1045671796@qq.com>
2018-02-03 10:00:04 +0800

    find annotation

--- index.js
+++ index.js
@@ -8,23 +8,25 @@ var Default = {
 }
 
 module.exports = postcss.plugin('postcss-px-to-vw', function (options) {
- 
     return function (css) {
         options = options || {};
-
         var opts = Object.assign({}, Default, options);
 
         css.walkRules(function (rule) {
-            rule.walkDecls(function (decl, i) {
-                if (decl.prop === 'font-size' || (decl.prop.indexOf('border') != -1)) return;
-                if (decl.value.indexOf('px') === -1) return;
+            rule.walkDecls(function(decl) {
 
-                decl.value = decl.value.replace(pxRegex, function(pxSize) {
+                const annotation = decl.next();
+                if ( annotation && annotation.type == 'comment' && annotation.text == 'px') return;
+
+                if (decl.value.indexOf('px') != 1) {
+                    decl.value = decl.value.replace(pxRegex, function (pxSize) {
                     var num = parseInt(pxSize);
                     var vwNum = num * 100 / opts.vwUnit;
                     return vwNum + 'vw';
-                });
-            });
+                    });
+                }
+            })
+            
         });
     }
 });
